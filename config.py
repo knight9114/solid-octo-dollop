0e22422edbd892d53ce0b5048eb405912d4275ce
Ian <ian.dahlstrom@gmail.com>
2022-04-20 12:57:41 -0400

    Part way through part 4

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
 import os
+from pathlib import Path
 
 @dataclass
 class Config:
     SECRET_KEY: str = os.environ.get('SECRET_KEY') or 'default-secret-key'
+    SQLALCHEMY_DATABASE_URI: str = os.environ.get('DATABASE_URL') or \
+        f'sqlite:///{(Path(__file__).parent / "app.db").resolve().absolute()}'
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
