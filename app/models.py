fb32f1091b8e3e2f58946864704de57d3a3ef864
Ian <ian.dahlstrom@gmail.com>
2022-04-20 19:24:11 -0400

    Partial progress on part 5

--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,6 +12,14 @@ class User(db.Model):
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
+
+    
+    def set_password(self, password: str):
+        self.password_hash = generate_password_hash(password)
+
+
+    def check_password(self, password: str) -> bool:
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
@@ -20,3 +30,8 @@ class Post(db.Model):
 
     def __repr__(self) -> str:
         return f'<Post {self.body}>'
+
+
+@login.user_loader
+def load_user(id: int | str) -> User:
+    return User.query.get(int(id))
